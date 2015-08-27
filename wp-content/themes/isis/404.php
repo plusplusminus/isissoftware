fb9ae13fab78972ac974f8bf9f17c8191ae5a7df
Leana10 <leana@plusplusminus.co.za>
2015-08-27 12:28:08 +0200

    404

---
+++ wp-content/themes/isis/404.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+     <section class="section_product css-quarter">
+	<div class="background-image-holder">
+		<?php // the_post_thumbnail('full',array('class'=>'background-image')); ?>
+	</div>
+	<div class="section_product--info">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="product_content">
+						<h2 class="product_content--title">404></h2>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section_product-single">  
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				
+				<p> This page is not available</p>
+
+		</div><!--/.row-->
+	</div>	<!--/.container-->
+</section>
+
+<?php get_footer(); ?>
